bafe9760114542d9be78698e6682bfd7f1d206a9
Gabriel <fuckU>
2016-03-14 22:50:41 -0300

    Calculadora de somar

---
+++ CalculaDroid/app/src/main/java/bo/CalculadoraBO.java
@@ -0,0 +1,11 @@
+package bo;
+
+/**
+ * Created by rm72468 on 14/03/2016.
+ */
+public class CalculadoraBO {
+
+    public static double somar(double numero1, double numero2){
+      return numero1+numero2;
+    }
+}
